363fba56e7a3f8156c7da460ee96662e16f578a0
MaryaSamsonenko <84477591+MaryaSamsonenko@users.noreply.github.com>
2021-11-01 22:25:46 +0200

    add solution task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const bodyEl = document.querySelector('body');
+const btnStartEl = document.querySelector('button[data-start]');
+const btnStopEl = document.querySelector('button[data-stop]');
 
+btnStartEl.addEventListener('click', () => {
+  timerChangeColor.start();
+});
+btnStopEl.addEventListener('click', () => {
+  timerChangeColor.stop();
+});
+
+const timerChangeColor = {
+  intervalId: null,
+
+  start() {
+    btnStartEl.disabled = true;
+
+    this.intervalId = setInterval(() => {
+      bodyEl.style.backgroundColor = getRandomRGBColor();
+    }, 1000);
+  },
+  stop() {
+    clearInterval(this.intervalId);
+
+    btnStartEl.disabled = false;
+  },
+};
+
+function getRandomIntegerFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function getRandomRGBColor() {
+  return `rgb(${getRandomIntegerFromInterval(0, 255)},
+    ${getRandomIntegerFromInterval(0, 255)},
+    ${getRandomIntegerFromInterval(0, 255)})`;
+}
